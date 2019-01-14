9cf62b085613ff2136730312c5a43d54230e2ac9
Viktor Eikman <viktor.eikman@gmail.com>
2019-01-14 22:00:46 +0100

    Added GPL notice to __init__.py.

--- snisku/__init__.py
+++ snisku/__init__.py
@@ -1,7 +1,26 @@
-"""The Snisku package."""
+# -*- coding: utf-8 -*-
+"""Files in this directory constitute the source code of Snisku.
+
+Snisku is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Snisku is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Snisku.  If not, see <http://www.gnu.org/licenses/>.
+
+Copyright 2019 Viktor Eikman.
+
+"""
 
 from . import exc
 from . import param
+from . import types
 
-__all__ = (exc, param)
+__all__ = (exc, param, types)
 __version__ = '0.1.0-alpha1'
